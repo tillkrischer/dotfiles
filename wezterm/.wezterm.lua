a55d4cb53017690df3e426bf58054cea06cfe2b5
Till Krischer <till@krischer.io>
2024-12-22 20:37:17 +0100

    wezterm

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,20 @@
+local wezterm = require 'wezterm'
+local config = {}
+config.font = wezterm.font 'JetBrains Mono'
+config.color_scheme = "Tokyo Night"
+-- config.color_scheme = 'Catppuccin Latte'
+-- config.enable_tab_bar = false
+config.use_fancy_tab_bar = false
+config.harfbuzz_features = {"calt=0", "clig=0", "liga=0"}
+config.default_prog = { 'C:/Program Files/PowerShell/7/pwsh.exe' }
+config.keys = {
+  {
+    key = ' ',
+    mods = 'CTRL',
+    action = wezterm.action.SendKey {
+      key = ' ',
+      mods = 'CTRL',
+    },
+  }
+}
+return config
