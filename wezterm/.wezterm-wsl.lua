dff668e84a1864a84d7b207deaffda4c7711e491
Till Krischer <till@krischer.io>
2024-12-23 09:59:20 +0100

    wsl cd

--- wezterm/.wezterm-wsl.lua
+++ wezterm/.wezterm-wsl.lua
@@ -1,5 +1,5 @@
-local base_config = require 'wezterm-base'
+local base_config = require 'dotfiles.wezterm.wezterm-base'
 
-base_config.default_prog = { 'C:/Windows/System32/wsl.exe', '-d', 'Ubuntu-24.04' }
+base_config.default_prog = { 'C:/Windows/System32/wsl.exe', '-d', 'Ubuntu-24.04', '--cd', '~'}
 
 return base_config
