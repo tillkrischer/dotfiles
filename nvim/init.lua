e641ce703e9d831144e98510fba9b63c4df6d8dc
Till Krischer <till.krischer@materna.de>
2025-01-02 10:41:30 +0100

    disable tree

--- nvim/init.lua
+++ nvim/init.lua
@@ -1032,28 +1032,28 @@ require("lazy").setup({
 			require("copilot_cmp").setup()
 		end,
 	},
-	{
-		"nvim-neo-tree/neo-tree.nvim",
-		version = "*",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-			"MunifTanjim/nui.nvim",
-		},
-		cmd = "Neotree",
-		keys = {
-			{ "\\", ":Neotree reveal<CR>", desc = "NeoTree reveal", silent = true },
-		},
-		opts = {
-			filesystem = {
-				window = {
-					mappings = {
-						["\\"] = "close_window",
-					},
-				},
-			},
-		},
-	},
+	-- {
+	-- 	"nvim-neo-tree/neo-tree.nvim",
+	-- 	version = "*",
+	-- 	dependencies = {
+	-- 		"nvim-lua/plenary.nvim",
+	-- 		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+	-- 		"MunifTanjim/nui.nvim",
+	-- 	},
+	-- 	cmd = "Neotree",
+	-- 	keys = {
+	-- 		{ "\\", ":Neotree reveal<CR>", desc = "NeoTree reveal", silent = true },
+	-- 	},
+	-- 	opts = {
+	-- 		filesystem = {
+	-- 			window = {
+	-- 				mappings = {
+	-- 					["\\"] = "close_window",
+	-- 				},
+	-- 			},
+	-- 		},
+	-- 	},
+	-- },
 	{
 		"sindrets/diffview.nvim",
 		dependencies = {
