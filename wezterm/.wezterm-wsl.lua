b62db182695befe4355c1ea627b087ebb45f429e
Till Krischer <till.krischer@materna.de>
2025-01-10 15:39:35 +0100

    better wsl

--- wezterm/.wezterm-wsl.lua
+++ wezterm/.wezterm-wsl.lua
@@ -1,5 +1,9 @@
+local wezterm = require("wezterm")
+local workspace_switcher = wezterm.plugin.require("https://github.com/MLFlexer/smart_workspace_switcher.wezterm")
+
 local base_config = require 'dotfiles.wezterm.wezterm-base'
 
-base_config.default_prog = { 'C:/Windows/System32/wsl.exe', '-d', 'Ubuntu-24.04', '--cd', '~'}
+base_config.default_domain = 'WSL:Ubuntu-24.04'
+workspace_switcher.zoxide_path = "wsl zoxide"
 
 return base_config
