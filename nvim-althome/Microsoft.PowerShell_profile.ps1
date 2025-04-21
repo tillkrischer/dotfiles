4550093f05ab25d52c71767e3a1eb65e019c82b5
Till Krischer <till@krischer.io>
2025-04-21 10:19:38 +0200

    clean

--- nvim-althome/Microsoft.PowerShell_profile.ps1
+++
@@ -1,8 +0,0 @@
-function nvim-althome {
-    $oldvalue = $env:LOCALAPPDATA
-    $env:LOCALAPPDATA = "C:\althome"
-    & "C:\Program Files\Neovim\bin\nvim.exe" $args
-    $env:LOCALAPPDATA = $oldvalue 
-}
-
-Set-Alias nvim nvim-althome
