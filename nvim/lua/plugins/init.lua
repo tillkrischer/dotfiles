7ec22d6b5d107b4b97f3756fa9ed44b748725f12
Till Krischer <till.krischer@materna.de>
2025-04-24 12:14:19 +0200

    colorizer

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -66,4 +66,11 @@ return {
       { "grf", function() require("conform").format({ async = true, lsp_format = "fallback" }) end, desc = "Format" },
     },
   },
+  {
+    "catgoose/nvim-colorizer.lua",
+    opts = {},
+    cmd = {
+      "ColorizerToggle",
+    },
+  }
 }
