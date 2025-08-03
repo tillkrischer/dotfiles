2dc96c65cf34b8dc69d2ebd44a8a73042d2a9610
Till Krischer <till.krischer@materna.de>
2025-08-03 21:10:39 +0200

    switcher

--- env/wezterm/workspace-switcher.lua
+++ env/wezterm/workspace-switcher.lua
@@ -1,10 +1,26 @@
 local wezterm = require("wezterm")
 local workspace_switcher = wezterm.plugin.require("https://github.com/MLFlexer/smart_workspace_switcher.wezterm")
 
+-- wezterm.on("update-right-status", function(window)
+--   local formatted_status = wezterm.format({
+--     { Background = { Color = "#dce0e8" } },
+--     { Foreground = { Color = "#8839ef" } },
+--     { Text = '' },
+--     { Background = { Color = "#8839ef" } },
+--     { Foreground = { Color = "#eff1f5" } },
+--     { Text = window:active_workspace() },
+--   })
+--
+--   window:set_right_status(formatted_status)
+-- end)
+
 wezterm.on("update-right-status", function(window)
-  window:set_right_status(window:active_workspace())
+  window:set_right_status(window:active_workspace() )
 end)
 
+if wezterm.target_triple == 'aarch64-apple-darwin' then
+  workspace_switcher.zoxide_path = "/opt/homebrew/bin/zoxide"
+end
 
 local config = {
   keys = {
