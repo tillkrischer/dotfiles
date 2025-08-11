f29cbc3913eae8e7f6919db9ff0e8bbe6c830460
Till Krischer <till.krischer@materna.de>
2025-08-11 21:43:32 +0200

    pin copilot chat

--- env/nvim/lua/plugins/copilot.lua
+++ env/nvim/lua/plugins/copilot.lua
@@ -13,16 +13,24 @@ return {
   },
   {
     "CopilotC-Nvim/CopilotChat.nvim",
+    version = "3.12.2",
+    -- version = "4.4.0",
     dependencies = {
       { "zbirenbaum/copilot.lua", },
       { "nvim-lua/plenary.nvim",  branch = "master" },
     },
     opts = {
-      model = "gpt-4.1"
+      model = "gpt-4.1",
+      mappings = {
+        complete = {
+          insert = "<Tab>",
+        },
+      },
     },
     cmd = {
       "CopilotChat",
       "CopilotChatToggle",
     },
+
   },
 }
