4aa51979a90a79f12a8e6e954f714eb355021e17
Till Krischer <till.krischer@materna.de>
2025-11-14 22:02:02 +0100

    @

--- env/nvim/lua/plugins/gitsigns.lua
+++ env/nvim/lua/plugins/gitsigns.lua
@@ -51,7 +51,7 @@ return {
         end, { desc = 'git [b]lame line' })
         map('n', '<leader>hd', gitsigns.diffthis, { desc = 'git [d]iff against index' })
         map('n', '<leader>hD', function()
-          gitsigns.diffthis '~'
+          gitsigns.diffthis '@'
         end, { desc = 'git [D]iff against last commit' })
         -- Toggles
         map('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = '[T]oggle git show [b]lame line' })
