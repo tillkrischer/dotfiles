805e1b10c7d0a7d148c63408a78adb51a4732985
Till Krischer <till.krischer@materna.de>
2026-04-16 15:30:22 +0200

    curl

--- runs/lsp.sh
+++ runs/lsp.sh
@@ -29,7 +29,7 @@ roslyn_url="https://pkgs.dev.azure.com/azure-public/vside/_packaging/vs-impl/nug
 roslyn_tmp="/tmp/${roslyn_package}"
 mkdir -p "$HOME/.local/bin"
 mkdir -p "$HOME/.local/share/roslyn_ls"
-wget -O "$roslyn_tmp" "$roslyn_url"
+curl -fL -o "$roslyn_tmp" "$roslyn_url"
 unzip -o "$roslyn_tmp" -d "$HOME/.local/share/roslyn_ls/package"
 cat > "$HOME/.local/bin/roslyn-language-server" <<EOF
 #!/usr/bin/env sh
