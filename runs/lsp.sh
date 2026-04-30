2c29d227ffcd5e39a9bd6965d2206c14ff4bb5f4
Till Krischer <till.krischer@materna.de>
2026-04-30 11:42:37 +0200

    roslyn install

--- runs/lsp.sh
+++ runs/lsp.sh
@@ -5,35 +5,9 @@ npm i -g vscode-langservers-extracted
 npm i -g graphql-language-service-cli
 npm i -g @github/copilot-language-server
 npm i -g @typescript/native-preview
+# curl -s "https://pkgs.dev.azure.com/azure-public/3ccf6661-f8ce-4e8a-bb2e-eff943ddd3c7/_packaging/3c18fd2c-cc7c-4cef-8ed7-20227ab3275b/nuget/v3/flat2/roslyn-language-server/index.json" | jq -r '.versions[0]'
+dotnet tool install --global roslyn-language-server --source https://pkgs.dev.azure.com/azure-public/vside/_packaging/vs-impl/nuget/v3/index.json --version "5.8.0-1.26229.18"
 
 # arch
 pacman -S lua-language-server
 
-
-# roslyn-language-server  (osx-arm64/linux-x64)
-# https://dev.azure.com/azure-public/vside/_artifacts/feed/vs-impl/NuGet/Microsoft.CodeAnalysis.LanguageServer.<platform>/overview
-roslyn_version="5.4.0-2.26179.14"
-case "$(uname -s)" in
-  Linux)
-    roslyn_platform="linux-x64"
-    ;;
-  Darwin)
-    roslyn_platform="osx-arm64"
-    ;;
-  *)
-    echo "Unsupported platform: $(uname -s)" >&2
-    exit 1
-    ;;
-esac
-roslyn_package="microsoft.codeanalysis.languageserver.${roslyn_platform}.${roslyn_version}.nupkg"
-roslyn_url="https://pkgs.dev.azure.com/azure-public/vside/_packaging/vs-impl/nuget/v3/flat2/microsoft.codeanalysis.languageserver.${roslyn_platform}/${roslyn_version}/${roslyn_package}"
-roslyn_tmp="/tmp/${roslyn_package}"
-mkdir -p "$HOME/.local/bin"
-mkdir -p "$HOME/.local/share/roslyn_ls"
-curl -fL -o "$roslyn_tmp" "$roslyn_url"
-unzip -o "$roslyn_tmp" -d "$HOME/.local/share/roslyn_ls/package"
-cat > "$HOME/.local/bin/roslyn-language-server" <<EOF
-#!/usr/bin/env sh
-exec dotnet "$HOME/.local/share/roslyn_ls/package/content/LanguageServer/${roslyn_platform}/Microsoft.CodeAnalysis.LanguageServer.dll" "\$@"
-EOF
-chmod +x "$HOME/.local/bin/roslyn-language-server"
