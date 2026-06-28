2a668709a0b3fb828ecf3c87cbe3e4ad9bf9ad64
Till Krischer <till.krischer@materna.de>
2026-06-28 15:49:02 +0200

    node 26

--- runs/mac/programs/cli/node.sh
+++ runs/mac/programs/cli/node.sh
@@ -5,5 +5,5 @@ install_node() {
     "https://nodejs.org/dist/v26.4.0/node-v26.4.0-darwin-arm64.tar.gz" \
     "4f4fbcacf6b1ff1a95deedba7bd7b2d79efecaa53a8ecb0530546dc9063fefbc" \
     "node-v26.4.0-darwin-arm64" \
-    "bin/node:node,bin/npm:npm,bin/npx:npx,bin/corepack:corepack"
+    "bin/node:node,bin/npm:npm,bin/npx:npx"
 }
