ca0e1603598e4a63f50e31a6f7b7dbfda7fced1c
Till Krischer <till.krischer@materna.de>
2026-07-06 16:48:00 +0200

    node + neovim verion

--- runs/mac/programs/cli/neovim.sh
+++ runs/mac/programs/cli/neovim.sh
@@ -1,9 +1,9 @@
 install_neovim() {
   install_archive_prefix \
     "neovim" \
-    "0.12.3" \
-    "https://github.com/neovim/neovim/releases/download/v0.12.3/nvim-macos-arm64.tar.gz" \
-    "532da1d00e465a660fa01c3d4991333d09c52107dce7df937368545daca0a14e" \
+    "0.12.4" \
+    "https://github.com/neovim/neovim/releases/download/v0.12.4/nvim-macos-arm64.tar.gz" \
+    "51ab83afa66d663627c2ab1be43209b0f4e81360d4598b53efaa4d8195f24c89" \
     "nvim-macos-arm64" \
     "bin/nvim:nvim"
 }
--- runs/mac/programs/cli/node.sh
+++ runs/mac/programs/cli/node.sh
@@ -1,9 +1,9 @@
 install_node() {
   install_archive_prefix \
     "node" \
-    "26.4.0" \
-    "https://nodejs.org/dist/v26.4.0/node-v26.4.0-darwin-arm64.tar.gz" \
-    "4f4fbcacf6b1ff1a95deedba7bd7b2d79efecaa53a8ecb0530546dc9063fefbc" \
-    "node-v26.4.0-darwin-arm64" \
+    "24.18.0" \
+    "https://nodejs.org/dist/v24.18.0/node-v24.18.0-darwin-arm64.tar.gz" \
+    "e1a97e14c99c803e96c7339403282ea05a499c32f8d83defe9ef5ec66f979ed1" \
+    "node-v24.18.0-darwin-arm64" \
     "bin/node:node,bin/npm:npm,bin/npx:npx"
 }
