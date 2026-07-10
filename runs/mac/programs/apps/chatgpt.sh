eaea368acd8699128d33605e3f9a60ec29405dec
Till Krischer <till.krischer@materna.de>
2026-07-10 09:50:48 +0200

    chatgpt app

---
+++ runs/mac/programs/apps/chatgpt.sh
@@ -0,0 +1,8 @@
+install_chatgpt() {
+  install_app_dmg \
+    "chatgpt" \
+    "2026-07-10" \
+    "https://persistent.oaistatic.com/codex-app-prod/ChatGPT.dmg" \
+    "6f67af7e2f934093ab8afebcec11374d40c8db8f9100fb6620f24155401d8319" \
+    "ChatGPT.app"
+}
