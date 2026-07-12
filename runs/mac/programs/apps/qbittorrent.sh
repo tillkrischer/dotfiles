b844cd64e8c339ad137aeef7cae31cee4d8122a6
Till Krischer <till@krischer.io>
2026-07-12 14:32:50 +0200

    qbit

---
+++ runs/mac/programs/apps/qbittorrent.sh
@@ -0,0 +1,8 @@
+install_qbittorrent() {
+  install_app_dmg \
+    "qbittorrent" \
+    "5.2.3" \
+    "https://downloads.sourceforge.net/project/qbittorrent/qbittorrent-mac/qbittorrent-5.2.3/qbittorrent-5.2.3.dmg" \
+    "9e37f6c7ff848c7bdd3c10167614c0cb78c00e2ddcc323f1ad3ac6c008a0481f" \
+    "qBittorrent.app"
+}
