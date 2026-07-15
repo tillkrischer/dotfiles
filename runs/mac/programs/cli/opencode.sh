25d7aa2392976ab5755ba3de0280845f1c52f9ab
Till Krischer <till.krischer@materna.de>
2026-07-15 10:40:55 +0200

    update oc

--- runs/mac/programs/cli/opencode.sh
+++ runs/mac/programs/cli/opencode.sh
@@ -1,9 +1,9 @@
 install_opencode() {
   install_archive_binary \
     "opencode" \
-    "1.17.10" \
-    "https://registry.npmjs.org/opencode-darwin-arm64/-/opencode-darwin-arm64-1.17.10.tgz" \
-    "8837811a5bb35b9a52bfe6e943f7881b95bcfa4a7444a5292181fb651ef9f18e" \
+    "1.18.1" \
+    "https://registry.npmjs.org/opencode-darwin-arm64/-/opencode-darwin-arm64-1.18.1.tgz" \
+    "b718d2f44ad84d02d01dc1b9d4288eb50b7ed4d588e29affe81e4a0622c2e8f19bac99b328d3f7bf0bcd8be475621d62e7fef0fd386b5b6393ca467696754539" \
     "package/bin/opencode" \
     "opencode"
 }
