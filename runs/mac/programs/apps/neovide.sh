926153d4523babd83f969fc538ddd3ab1489608e
Till Krischer <till.krischer@materna.de>
2026-07-15 10:40:53 +0200

    vide

---
+++ runs/mac/programs/apps/neovide.sh
@@ -0,0 +1,8 @@
+install_neovide() {
+  install_app_dmg \
+    "neovide" \
+    "0.16.2" \
+    "https://github.com/neovide/neovide/releases/download/0.16.2/Neovide-aarch64-apple-darwin.dmg" \
+    "5eb745ea7f20cf00491a8ffd8d793c6169ef1c6e1a0f75ae8be40571e65c6b26" \
+    "Neovide.app"
+}
