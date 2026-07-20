10581d588feed568f32ca14667faf5f874d34973
Till Krischer <till.krischer@materna.de>
2026-07-20 08:35:15 +0200

    zig

---
+++ runs/mac/programs/cli/zig.sh
@@ -0,0 +1,9 @@
+install_zig() {
+  install_archive_prefix \
+    "zig" \
+    "0.16.0" \
+    "https://ziglang.org/download/0.16.0/zig-aarch64-macos-0.16.0.tar.xz" \
+    "b23d70deaa879b5c2d486ed3316f7eaa53e84acf6fc9cc747de152450d401489" \
+    "zig-aarch64-macos-0.16.0" \
+    "zig:zig"
+}
