e616e0a93914f36b8ea1e64e2d8d621728362aa6
Till Krischer <till@krischer.io>
2026-08-08 18:59:43 +0200

    herdr

---
+++ runs/mac/programs/cli/herdr.sh
@@ -0,0 +1,8 @@
+install_herdr() {
+  install_raw_binary \
+    "herdr" \
+    "0.8.0" \
+    "https://github.com/herdrdev/herdr/releases/download/v0.8.0/herdr-macos-aarch64" \
+    "d53a9f93fccfdfcc55632927bf51002f5add0aa7990bcdf508ffbd84ac658178" \
+    "herdr"
+}
